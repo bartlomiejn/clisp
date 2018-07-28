31104c9cb26e3dc4d79c4a09c06e7c68507c5e80
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2018-07-28 21:02:18 +0200

    Added select by artist

--- cddb.lisp
+++ cddb.lisp
@@ -40,3 +40,13 @@
     (with-standard-io-syntax
       (print *db* out))))
 
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax
+      (setf *db* (read in)))))
+
+(defun select-by-artist (artist)
+  (remove-if-not
+   #'(lambda (cd) (equal (getf cd :artist) artist))
+   *db*))
+
